e3dce779895e2ec8704d1b9106d33901bdf5ac92
Darkkii <anton_saarinen@hotmail.com>
2024-09-26 11:50:42 +0300

    tentative format for sensors

---
+++ src/sensor/BaseSensor.hpp
@@ -0,0 +1,15 @@
+#ifndef BASESENSOR_HPP
+#define BASESENSOR_HPP
+
+namespace Sensor
+{
+
+class BaseSensor
+{
+  public:
+    virtual void update();
+};
+
+} // namespace Sensor
+
+#endif /* BASESENSOR_HPP */
