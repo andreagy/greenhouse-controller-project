44481d0788803eff9a91cb76012f7846fed5ed5f
Darkkii <anton_saarinen@hotmail.com>
2024-09-26 12:11:26 +0300

    move public before private

--- src/sensor/SDP600.hpp
+++ src/sensor/SDP600.hpp
@@ -9,6 +9,11 @@ namespace Sensor
 
 class SDP600
 {
+  public:
+    SDP600(i2c_inst *i2c = i2c1, uint SDA_pin = 14, uint SCL_pin = 15, uint8_t devAddr = 0x40);
+    void update();
+    int16_t getPressure() const;
+
   private:
     i2c_inst *mSensor_i2c;
     uint8_t mDevAddr;
@@ -18,11 +23,6 @@ class SDP600
         uint16_t mUint16;
         int16_t mInt16;
     } mPressure;
-
-  public:
-    SDP600(i2c_inst *i2c = i2c1, uint SDA_pin = 14, uint SCL_pin = 15, uint8_t devAddr = 0x40);
-    void update();
-    int16_t getPressure() const;
 };
 
 } // namespace Sensor
