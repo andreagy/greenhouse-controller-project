9497e002f9c475bc226e8efe4086c973b570384a
Darkkii <anton_saarinen@hotmail.com>
2024-09-26 12:11:42 +0300

    tentative class for sensor reader

--- src/task/SensorReader.hpp
+++ src/task/SensorReader.hpp
@@ -2,5 +2,25 @@
 #define SENSORREADER_HPP
 
 // TODO: Trigger reading from modbus and update sensor values to relevant classes
+#include "sensor/BaseSensor.hpp"
+#include "task/BaseTask.hpp"
+
+#include <memory>
+#include <vector>
+
+namespace Task
+{
+class SensorReader : public BaseTask
+{
+  public:
+    SensorReader();
+    void attach(std::shared_ptr<Sensor::BaseSensor>);
+    void run();
+
+  private:
+    std::vector<std::shared_ptr<Sensor::BaseSensor>> m_Sensors;
+    void update();
+};
+} // namespace Task
 
 #endif /* SENSORREADER_HPP */
