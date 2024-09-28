8d2f1af6ee4183624d1c39b0ac46d56cc921ddaa
Darkkii <anton_saarinen@hotmail.com>
2024-09-29 00:18:30 +0300

    add static function for type punning

--- src/sensor/Register32bit.hpp
+++ src/sensor/Register32bit.hpp
@@ -6,12 +6,22 @@
 namespace Sensor
 {
 
-// Used for type punning the sensor data from integer to float.
+// Used for "type punning" the 32-bit unsigned integer to a floating point number.
 union Register32bit
 {
     uint32_t u;
     float f;
 };
+
+// "Type puns" 2x 16-bit unsigned integers into a floating point number using a union.
+static float convertToFloat(uint16_t values[2])
+{
+    constexpr uint8_t LSB = 0;
+    constexpr uint8_t MSB = 1;
+
+    Register32bit tmp{static_cast<uint32_t>(values[LSB] | (values[MSB] << 16))};
+    return tmp.f;
+}
 
 } // namespace Sensor
 
