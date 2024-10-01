7c467558756cc1e1eaeee395108cfd30f1a54b37
Darkkii <anton_saarinen@hotmail.com>
2024-10-01 11:17:44 +0300

    renamed file to prevent include guard clashes

--- src/task/fan/FanController.cpp
+++ src/task/fan/FanController.cpp
@@ -1,9 +1,12 @@
-#include "Controller.hpp"
+#include "FanController.hpp"
 
 #include "timers.h"
 
 #include <climits>
 #include <cstdint>
+
+namespace Task
+{
 
 namespace Fan
 {
@@ -14,8 +17,8 @@ constexpr int ROT_REG_ADDR = 0x0004;
 
 Controller::Controller(std::shared_ptr<Modbus::Client> modbus,
                        uint32_t stackDepth,
-                       Task::priority taskPriority) :
-    Task::BaseTask("FanController", stackDepth, this, taskPriority),
+                       priority taskPriority) :
+    BaseTask("FanController", stackDepth, this, taskPriority),
     m_SpeedRegister{modbus, MODBUS_ADDR, SPEED_REG_ADDR},
     m_PulseRegister{modbus, MODBUS_ADDR, ROT_REG_ADDR, false}
 {}
@@ -26,7 +29,9 @@ uint16_t Controller::getSpeed() { return m_Speed; }
 
 void Controller::setSpeed(uint16_t speed)
 {
-    if (speed < 0 || speed > 1000) { speed = speed < 1000 ? 0 : 1000; }
+    constexpr uint16_t FAN_MAX = 1000;
+
+    if (speed < 0 || speed > FAN_MAX) { speed = speed < FAN_MAX ? 0 : FAN_MAX; }
 
     m_SpeedRegister.write(speed);
     m_Speed = m_SpeedRegister.read();
@@ -34,13 +39,16 @@ void Controller::setSpeed(uint16_t speed)
 
 void Controller::run()
 {
-    uint32_t newSpeed = 0;
+    uint32_t speed = 0;
+    // setSpeed(500);
 
     while (true)
     {
-        xTaskNotifyWait(0, ULONG_MAX, &newSpeed, portMAX_DELAY);
-        setSpeed(static_cast<uint16_t>(newSpeed));
+        xTaskNotifyWait(0, ULONG_MAX, &speed, portMAX_DELAY);
+        setSpeed(static_cast<uint16_t>(speed));
     }
 }
 
 } // namespace Fan
+
+} // namespace Task
