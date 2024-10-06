21caf65e61c8679f528af86ee033af3e8ba39b25
Darkkii <anton_saarinen@hotmail.com>
2024-10-06 20:59:28 +0300

    split timeout into two variations, use tick counts instead of timers

---
+++ src/timer/CounterTimeout.cpp
@@ -0,0 +1,24 @@
+#include "CounterTimeout.hpp"
+
+#include "FreeRTOS.h" // IWYU pragma: keep
+#include "projdefs.h"
+#include "task.h"
+
+#include <cstdint>
+
+namespace Timer
+{
+
+CounterTimeout::CounterTimeout(uint32_t ms) :
+    m_Timeout{ms},
+    m_Timestamp{xTaskGetTickCount()}
+{}
+
+void CounterTimeout::reset() { m_Timestamp = xTaskGetTickCount(); }
+
+bool CounterTimeout::operator()()
+{
+    return xTaskGetTickCount() - m_Timestamp > pdMS_TO_TICKS(m_Timeout);
+}
+
+} // namespace Timer
---
+++ src/timer/CounterTimeout.hpp
@@ -0,0 +1,25 @@
+#ifndef COUNTERTIMEOUT_HPP
+#define COUNTERTIMEOUT_HPP
+
+#include "portmacro.h"
+
+#include <cstdint>
+
+namespace Timer
+{
+
+class CounterTimeout
+{
+  public:
+    CounterTimeout(uint32_t ms);
+    void reset();
+    bool operator()();
+
+  private:
+    uint32_t m_Timeout;
+    TickType_t m_Timestamp;
+};
+
+} // namespace Timer
+
+#endif /* COUNTERTIMEOUT_HPP */
