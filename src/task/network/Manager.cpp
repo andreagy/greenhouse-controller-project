c14b40e87b7a35fb6f5db18d1cbc4b7e920795e6
Darkkii <anton_saarinen@hotmail.com>
2024-10-07 21:02:35 +0300

    added eeprom management to "network manager"

--- src/task/network/Manager.cpp
+++ src/task/network/Manager.cpp
@@ -1,7 +1,8 @@
 #include "Manager.hpp"
 
+#include "network/NetHeader.hpp"
 #include "network/TlsClient.hpp"
-#include "portmacro.h"
+#include "storage/Eeprom.hpp"
 #include "task/BaseTask.hpp"
 #include <cyw43_ll.h>
 #include <hardware/gpio.h>
@@ -10,49 +11,69 @@
 #include <pico/error.h>
 
 #include <cstdio>
+#include <memory>
 #include <string>
-
-// Testing purposes
-const std::string TLS_CLIENT_HTTP_REQUEST =
-    "POST /update HTTP/1.1\r\n"
-    "Host: api.thingspeak.com\r\n"
-    "User-Agent: PicoW\r\n"
-    "Accept: */*\r\n"
-    "Content-Length: 35\r\n"
-    "Content-Type: application/x-www-form-urlencoded\r\n"
-    "\r\n"
-    "api_key=" CHANNEL_API_KEY "talkback_key=" TALKBACK_API_KEY "field1="
-    + std::to_string(12)              /* co2 (ppm) */
-    + "field2=" + std::to_string(12)  /* rh (%) */
-    + "field3=" + std::to_string(12)  /* temp (C) */
-    + "field4=" + std::to_string(12)  /* speed (%) */
-    + "field5=" + std::to_string(12); /* target (ppm) */
 
 namespace Task
 {
 namespace Network
 {
 
-Manager::Manager() : BaseTask{"NetworkClient", 1024, this, MED} {}
+Manager::Manager(std::shared_ptr<Sensor::GMP252> co2Sensor,
+                 std::shared_ptr<Sensor::HMP60> rhSensor,
+                 std::shared_ptr<Task::Co2::Controller> co2Controller,
+                 std::shared_ptr<Task::Fan::Controller> fanController,
+                 std::shared_ptr<Storage::Eeprom> eeprom,
+                 QueueHandle_t targetQueue,
+                 QueueHandle_t settingsQueue) :
+    BaseTask{"NetworkClient", 1024, this, MED},
+    m_Co2Sensor{co2Sensor},
+    m_RhSensor{rhSensor},
+    m_Co2Controller{co2Controller},
+    m_FanController{fanController},
+    m_Eeprom{eeprom},
+    m_ReportTimeout{15000},
+    m_TargetQueue{targetQueue},
+    m_SettingsQueue{settingsQueue}
+{
+    update();
+}
 
 void Manager::run()
 {
-    // TODO: get wifi settings from eeprom or environment
-    if (m_Ssid.empty()) { m_Ssid = WIFI_SSID; }
-    if (m_Password.empty()) { m_Password = WIFI_PASSWORD; }
+    if (!m_Eeprom->read(Storage::SSID_ADDR, m_Ssid) || m_Ssid.empty())
+    {
+        m_Ssid = WIFI_SSID;
+    }
+    if (!m_Eeprom->read(Storage::PASSWORD_ADDR, m_Password) || m_Password.empty())
+    {
+        m_Password = WIFI_PASSWORD;
+    }
+    if (!m_Eeprom->read(Storage::API_KEY_ADDR, m_ApiKey) || m_ApiKey.empty())
+    {
+        m_ApiKey = CHANNEL_API_KEY;
+    }
+    if (!m_Eeprom->read(Storage::TALKBACK_KEY_ADDR, m_TalkbackKey)
+        || m_TalkbackKey.empty())
+    {
+        m_TalkbackKey = TALKBACK_API_KEY;
+    }
 
     int err = connect();
 
     if (err == 0) { m_Connected = true; }
-    ::Network::Client tlsClient(30);
+    m_TlsClient = std::make_shared<::Network::Client>(30, m_TargetQueue);
 
     while (true)
     {
-        // TODO: handle sending thingspeak
-
-        // TODO: handle getting command from talkback -> send direct notify to co2 control
-
-        vTaskDelay(portMAX_DELAY);
+        if (m_Connected)
+        {
+            getTalkback();
+            vTaskDelay(5000);
+            update();
+            sendReport();
+            vTaskDelay(5000);
+        }
     }
 }
 
@@ -77,7 +98,64 @@ int Manager::connect()
     }
     else { printf("failed to initialise - error %d\n", err); }
 
+    if (err == 0)
+    {
+        m_Eeprom->write(Storage::SSID_ADDR, m_Ssid);
+        m_Eeprom->write(Storage::PASSWORD_ADDR, m_Password);
+        m_Eeprom->write(Storage::API_KEY_ADDR, m_ApiKey);
+        m_Eeprom->write(Storage::TALKBACK_KEY_ADDR, m_TalkbackKey);
+    }
+
     return err;
+}
+
+int Manager::reconnect(std::string ssid, std::string password)
+{
+    // TODO: Implement disconnecting and reconnecting while system is up
+    return 0;
+}
+
+void Manager::sendReport()
+{
+    if (m_ReportTimeout())
+    {
+        m_TlsClient->send(createRequest(m_Data));
+        m_ReportTimeout.reset();
+    }
+}
+
+void Manager::getTalkback() { m_TlsClient->send(createRequest()); }
+
+std::string Manager::createRequest(const ::Network::Data &data)
+{
+    std::string request = ::Network::Request::updateHeader;
+    std::string content = "api_key=" + m_ApiKey
+                          + "&field1=" + std::to_string(data.co2)
+                          + "&field2=" + std::to_string(data.rh)
+                          + "&field3=" + std::to_string(data.temp)
+                          + "&field4=" + std::to_string(data.speed)
+                          + "&field5=" + std::to_string(data.target);
+    request += std::to_string(content.size()) + ::Network::Request::headerEnd + content;
+
+    return request;
+}
+
+std::string Manager::createRequest()
+{
+    std::string request = ::Network::Request::executeHeader;
+    std::string content = "api_key=" + m_TalkbackKey;
+    request += std::to_string(content.size()) + ::Network::Request::headerEnd + content;
+
+    return request;
+}
+
+void Manager::update()
+{
+    m_Data.co2 = m_Co2Sensor->getCo2();
+    m_Data.rh = m_RhSensor->getRh();
+    m_Data.temp = m_RhSensor->getTemp();
+    m_Data.target = m_Co2Controller->getTarget();
+    m_Data.speed = m_FanController->getSpeed();
 }
 
 } // namespace Network
